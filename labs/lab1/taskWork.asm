3833ed17b7ea62c70a19d07b60e2fc58db364315
EssamShenhab <essam119999@gmail.com>
2025-10-18 10:30:09 +0300

    Upload lab1

---
+++ labs/lab1/taskWork.asm
@@ -0,0 +1,21 @@
+.data 
+name:   .asciiz "Name:   Essam Mohammed Essam Shenhab\n"
+id:     .asciiz "ID:     20812022100664\n"
+course: .asciiz "Course: CSE321-Computer-Organization\n"
+
+.text
+main:
+    li $v0, 4
+    la $a0, name
+    syscall
+
+    li $v0, 4
+    la $a0, id
+    syscall
+
+    li $v0, 4
+    la $a0, course
+    syscall
+
+    li $v0, 10
+    syscall
